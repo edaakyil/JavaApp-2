e5e8d63ae49f2d2953471be6fa3fe4625ca71d19
edaakyil <edaakyil@gmail.com>
2025-08-23 10:14:32 +0300

    Update

--- Projects/001-DemoThreadClass/src/main/java/com/edaakyil/java/app/Application.java
+++ Projects/001-DemoThreadClass/src/main/java/com/edaakyil/java/app/Application.java
@@ -3,5 +3,8 @@ package com.edaakyil.java.app;
 public class Application {
     public static void run(String[] args)
     {
+        var self = Thread.currentThread();
+
+        System.out.printf("Thread name: %s%n", self.getName());
     }
 }
